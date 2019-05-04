b357b771c3592c24dcfe2192d74054e621dd6773
Erfan Tahriri <erfan.tahriri@gmail.com>
2019-05-04 23:19:27 +0430

    Change database to PostgreSQL and add its configs
    Add psycopg2-binary to requirements file

--- planningpoker/settings.py
+++ planningpoker/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'planningpoker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('PLANNING_POKER_DB_NAME'),
+        'USER': os.environ.get('PLANNING_POKER_DB_USER'),
+        'PASSWORD': os.environ.get('PLANNING_POKER_DB_PASSWORD'),
+        'HOST': os.environ.get('PLANNING_POKER_DB_HOST'),
     }
 }
 
