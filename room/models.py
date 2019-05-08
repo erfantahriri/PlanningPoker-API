3dc25161c5caef12a84924a6e8e1903ff220eb94
Erfan Tahriri <erfan.tahriri@gmail.com>
2019-05-09 02:47:24 +0430

    Add Participant model and its migration file to room app

--- room/models.py
+++ room/models.py
@@ -1,3 +1,5 @@
+import jwt
+from django.conf import settings
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
@@ -21,4 +23,26 @@ class Room(BaseModel):
 
     def __str__(self):
         """returns id as Unicode “representation” of Room object."""
+        return self.uid
+
+
+class Participant(BaseModel):
+    """
+        Data model for Participant
+
+    """
+
+    room = models.ForeignKey(Room, verbose_name=_('Room'),
+                             on_delete=models.PROTECT)
+
+    name = models.CharField(verbose_name=_("name"), max_length=128)
+
+    class Meta:
+        verbose_name = _("Participant")
+        verbose_name_plural = _("Participants")
+        ordering = ("-created",)
+        unique_together = ['room', 'name']
+
+    def __str__(self):
+        """returns id as Unicode “representation” of Participant object."""
         return self.uid
