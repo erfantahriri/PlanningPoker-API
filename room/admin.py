93fc454ea7f49854618620d11f93b353bd71ec8a
Erfan Tahriri <erfan.tahriri@gmail.com>
2019-05-09 00:22:54 +0430

    Add Room admin pages to admin panel

--- room/admin.py
+++ room/admin.py
@@ -1,1 +1,12 @@
 from django.contrib import admin
+from room.models import Room
+
+
+class RoomAdmin(admin.ModelAdmin):
+
+    exclude = ()
+    list_display = ('uid', 'title', 'description', 'updated', 'created',)
+    readonly_fields = ('uid', 'updated', 'created',)
+
+
+admin.site.register(Room, RoomAdmin)
