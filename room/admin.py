d5c64100bf230ba7bce5d023ec8fd4e433ff6f97
Erfan Tahriri <erfan.tahriri@gmail.com>
2019-05-09 11:45:36 +0430

    Add Participants and Issues inline admins to Room admin page

--- room/admin.py
+++ room/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
-from room.models import Room
+from room.models import Room, Participant, Issue
+
+
+class ParticipantInlineAdmin(admin.TabularInline):
+
+    model = Participant
+    readonly_fields = ('uid', 'created',)
+    extra = 0
+
+
+class IssueInlineAdmin(admin.TabularInline):
+
+    model = Issue
+    readonly_fields = ('uid', 'created',)
+    extra = 0
 
 
 class RoomAdmin(admin.ModelAdmin):
@@ -7,6 +21,8 @@ class RoomAdmin(admin.ModelAdmin):
     exclude = ()
     list_display = ('uid', 'title', 'description', 'updated', 'created',)
     readonly_fields = ('uid', 'updated', 'created',)
+
+    inlines = [ParticipantInlineAdmin, IssueInlineAdmin]
 
 
 admin.site.register(Room, RoomAdmin)
