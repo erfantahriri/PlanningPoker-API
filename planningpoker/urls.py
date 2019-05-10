dac37ba2966fde09f2aa1250f374a9835e629556
Erfan Tahriri <erfan.tahriri@gmail.com>
2019-05-10 17:57:54 +0430

    Add API doc to project

--- planningpoker/urls.py
+++ planningpoker/urls.py
@@ -13,10 +13,28 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Planning Poker API",
+      default_version='v1',
+      description="Planning Poker API docs",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="erfan,tahriri@gmail.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('v1/rooms/', include('room.urls')),
+    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
