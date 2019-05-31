5b42d9592a1b49c5b9eceb138488c47c6efd4b93
Erfan Tahriri <erfan.tahriri@gmail.com>
2019-05-31 12:05:54 +0430

    Add name for room URLs to be able to reverse them use in test cases

--- room/urls.py
+++ room/urls.py
@@ -3,18 +3,21 @@ from django.urls import path
 from room import views
 
 urlpatterns = [
-    path('', views.RoomAPIView.as_view()),
+    path('', views.RoomAPIView.as_view(), name='rooms'),
     path('<str:room_uid>/join', views.JoinRoomAPIView.as_view(),
          name='join_room'),
     path('<str:room_uid>/participants',
-         views.RoomParticipantsListAPIView.as_view()),
-    path('<str:room_uid>/issues', views.RoomIssueAPIView.as_view()),
+         views.RoomParticipantsListAPIView.as_view(),
+         name='room_participants'),
+    path('<str:room_uid>/issues', views.RoomIssueAPIView.as_view(),
+         name='room_issues'),
     path('<str:room_uid>/current_issue',
-         views.RoomCurrentIssueAPIView.as_view()),
+         views.RoomCurrentIssueAPIView.as_view(), name='room_current_issue'),
     path('<str:room_uid>/issues/<str:uid>',
-         views.IssueAPIView.as_view()),
+         views.IssueAPIView.as_view(), name='room_issue'),
     path('<str:room_uid>/issues/<str:issue_uid>/votes',
-         views.VoteAPIView.as_view()),
+         views.VoteAPIView.as_view(), name='room_issue_votes'),
     path('<str:room_uid>/issues/<str:issue_uid>/votes/flip',
-         views.FlipIssueVoteCardsAPIView.as_view()),
+         views.FlipIssueVoteCardsAPIView.as_view(),
+         name='room_issue_flip_votes'),
 ]
